29e2aeda2d2e92bd486d289a7ac33b3ce673212a
wilson <wjuma19dev@gmail.com>
2024-07-26 16:48:07 -0400

    Start to learn PYTHON

---
+++ 01-basic/comment.py
@@ -0,0 +1,41 @@
+#MAKE A COMMENT
+
+## Wath if you wanted to include something in your code like a note?
+
+  ### **include # before the line** 
+  ### **this is called a comment**
+
+
+# h1
+## h2
+### h3
+
+# **Bold text**
+# > blockquote
+
+# Ordered List
+  # 1. First item
+  # 2. Second item
+  # 3. Third item
+
+# Unordered List
+  # - First item
+  # - Second item
+  # - Third item
+
+
+# `code`
+
+# Horizont rule ---
+
+# Link [Title](http://www.google.com)
+
+#image ![alt text](image.png)
+
+#this is a comment
+
+
+#symbol          name                       example                    usage explanation
+#  !             shell command              !ls                       Executes shell commands form the notebook cell, such as listing file.
+#  %             line magic                 %cd 01-basic              Execute a line magic command, affecting only the line it is on.
+#  %%            cell magic                 %%writefile text.txt      Applies the magic command to the entere cell.       
---
+++ 01-basic/create-variable.py
@@ -0,0 +1,13 @@
+company_name = 'Datawiz Inc.'
+job_wfh = True
+salary_rate = .10
+job_title = 'Data Analitic'
+base_salary = 100000
+job_salary = (base_salary) * (1+salary_rate)
+job_id = 1
+
+
+
+
+
+
